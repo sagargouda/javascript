d7079a4a16a601090abe54777878bf73e4a101a2
sagar <sagarc2580@gmail.com>
2023-12-12 13:31:32 +0530

    spread and rest operators done

--- Day-4/notes.md
+++ Day-4/notes.md
@@ -4,4 +4,59 @@
 
 3) when swapping use let because we are re assigning
 
-4) destructuring objects
+4) destructuring objects = in objects order of elemnts doesn't matter. it is used when we are getting sonmething from api calls
+
+5) //?? mutating variables (swapping as said before)
+
+let a = 11;
+let b = 22;
+const obj = { a: 23, b: 34, c: 34 };
+//\*_You can't do const becauise a and b are already declared and you can't do const {a,b} _/
+// we overrided it so use () because when you use {}, js wants code blocks so it gives error
+({ a, b } = obj);
+console.log(a, b);
+
+6. we can pass in a fucntion as an argument so we don't have to worry abut where parameters should be placed
+
+7) expanding an array we use spread and for passing something to a function
+
+8) spread operator works on all iterables in js even object ;
+
+9) we can't use spread in template literal , bcs it don't expect muiltiple values.
+
+10) we can make shallow copy in object using spread operator
+
+11) rest packs and spread unpacks , rest will condense elements into an array .
+
+12) rest is really rest of elements it always must be last in deswructring assignment
+
+13) Spread Operator vs Rest operator
+
+The main difference:
+
+=== Rest operator ===
+
+· The rest operator is used in places where we would otherwise write a data structure (such as an array or object). This is because the rest operator collects "loose" values into an array or object. Thus, they can be used in places where arrays or objects can be used, such as…
+
+Note: Given "loose" values, try to imagine them being scooped up into an array. The three dots say
+place any number of loose elements here and surround then with a leading and trailing square bracket such that a new array is subsequently formed".
+
+Function parameters (when declaring/ expressing a function)
+
+or...
+
+^ In this case funcString can be called with any number of elements where the first two are associated with their positional variables (a and b) and the leftover "loose" values are collected into an array named "manyMoreArgs".
+
+Left side of a destructuring assignment ( to collect individual elements into an object or an array which will be used to deconstruct the object or array on the right side of the assignment operator.
+
+=== Spread Operator ===
+
+· The spread operator is used in places where we would otherwise write values separated by commas. This is because the spread operator spreads elements into individual values. Thus, they can be used in places where individual elements can be used, such as…
+
+Note: With arrays, try to imagine the elements of the array being poured into a new location. The three dots say "place the array here, but remove the leading and trailing square brackets" such that "[1,2,3]" becomes " 1,2,3".
+
+Function arguments (when calling a function)
+
+In an array or object, on the right side of a destructuring assignment (to be assigned to the variables on the left side of the assignment operator).
+
+15. "Where we want to write some variables name, let's use rest" and "Where we want to write a list of values, one by one separated by commas, let's use spread")
--- Day-4/script.js
+++ Day-4/script.js
@@ -29,9 +29,117 @@ const restaurant = {
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+  orderDelivery: function ({ time, name, hot }) {
+    console.log(time, hot, name);
+  },
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is ${ing1},${ing2},${ing3}`);
+  },
 };
+//!! 4) Rest operator
+//rest
+// const [a, b, ...c] = [1, 2, 3, 4];
+//spread
+// console.log(a, b, ...c);
+
+//?? Objects (remaining elements will be collected in objects not arrays )
+
+// const { sat, ...weekdays } = restaurant.openingHours;
+// console.log(sat, weekdays);
+
+//?? functions
+
+// const add = function (...sagar) {
+//   let sum = 0;
+//   for (let i = 0; i < sagar.length; i++) {
+//     sum += sagar[i];
+//   }
+//   console.log(sum);
+// };
+
+// add(1, 2, 3, 4, 2);
+// add(1, 2);
+// add(12, 2, 2, 2, 3, 22, 2);
+
+///////////////////////////////////////////////////////////////////////////////////////////
+//!! 3) Spread operator
+
+//?? expanding an array and when passing something to funciton
+// const arr = [1, 2, 3];
+// const newAeer = [1, 2, ...arr];
+// console.log(...newAeer);
+
+// const newMenu = [...restaurant.mainMenu, 'idli', 'vada'];
+// console.log(...newMenu);
+
+//?? copy array
+// const mainMenu = [...restaurant.mainMenu];
+
+//?? joining 2 arrays;
+// const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+// console.log(menu);
+
+//??spread operator in stringn
+// const name = 'sagar';
+// console.log(...name, '', 'sss');
+
+//?? real example
+// const ing = ['pasta', 'idli', 'vada'];
+// restaurant.orderPasta(...ing);
+
+//?? Objects
+// const newRest = { ...restaurant, founder: 'sagar', foundedIn: '1911' };
+// console.log(newRest);
+
+// const restaurantCopy = { ...restaurant };
+// restaurantCopy.name = 'sgaar';
+// console.log(restaurantCopy.name);
+// console.log(restaurant.name);
+///////////////////////////////////////////////////////////////////////////////////
 
 //!! 2) destructuring objects
+
+// const { name, openingHours, categories } = restaurant;
+// console.log(name, openingHours, categories);
+
+//?? if i want to change variable names
+
+// const { name: restName, openingHours: open, categories: cat } = restaurant;
+// console.log(restName, open, cat);
+
+//?? if something doesn't exist and we are trying to access it (default values)
+
+// const { menu = [], starterMenu: starter = [] } = restaurant;
+// const [a, b, c, d] = starter;
+// console.log(menu);
+// console.log(a, b, c, d);
+
+//?? mutating variables (swapping as said before)
+
+// let a = 11;
+// let b = 22;
+// const obj = { a: 23, b: 34, c: 34 };
+//**You can't do const becauise a and b are already declared and you can't do const {a,b} */
+// we overrided it so use () because when you use {}, js wants code blocks so it gives error
+// ({ a, b } = obj);
+// console.log(a, b);
+
+//?? nested object
+// const {
+//   fri: { open: opening, close: closing },
+//   sat: { open: open, close: close },
+// } = restaurant.openingHours;
+// console.log(opening, closing);
+
+//?? passing object as argumenst
+
+// restaurant.orderDelivery({
+//   time: '12',
+//   name: 'sagar',
+//   hot: 'yes',
+// });
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //!! 1) destructuring array
 // const [a, b, c] = [2, 3, 4, 5];
